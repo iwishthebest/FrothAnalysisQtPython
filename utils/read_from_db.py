f7b1a642a13908741c7d93a922309bb75a4495b5
x <nozding@foxmail.com>
2025-11-10 11:23:13 +0800

    删除未使用的模块和文件，清理代码库

--- utils/read_from_db.py
+++
@@ -1,9 +0,0 @@
-
-import sqlite3
-import pandas as pd
-
-conn = sqlite3.connect("data_records.db")
-# 查询最近10条数据
-df = pd.read_sql("SELECT * FROM sensor_data ORDER BY id DESC LIMIT 100", conn)
-print(df)
-conn.close()
